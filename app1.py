2759e4e48563f0587c9d95313eed50dbacc2eef4
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-16 17:54:30 +0200

    consider uppercases and capital words; implement way to check a word close to the one from the user input exist in the dictonary

--- app1.py
+++ app1.py
@@ -1,10 +1,15 @@
 import json
+import difflib 
+from difflib import get_close_matches
 
 data = json.load(open("source_file/data.json"))
 
 def dictionary(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys()))>0:
+        return 'Did you mean %s instead?' % get_close_matches(word, data.keys())[0]
     else:
         return 'The word does not exist! Enter a new word!'
     
