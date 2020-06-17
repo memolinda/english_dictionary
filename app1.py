d77f5dde0833698f0642e19591d0f99aaccf0331
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-17 10:40:58 +0200

    change the output style; add the case for Paris and Dehli

--- app1.py
+++ app1.py
@@ -5,24 +5,33 @@ from difflib import get_close_matches
 data = json.load(open("source_file/data.json"))
 
 def dictionary(word):
-    word = word.lower()
-    if word in data:
+    if word == 'Delhi' or word == 'Paris':
         return data[word]
-    elif len(get_close_matches(word, data.keys()))>0:
-        yn = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(word, data.keys())[0])
-        if yn == 'Y' or yn == 'y':
-            return data[get_close_matches(word, data.keys())[0]]
-        elif yn == 'N' or yn == 'n':
-            return 'The word does not exist! Please check it!'
-        else:
-            return 'I did not understand the entry!'
     else:
-        return 'The word does not exist! Please check it!'
+        word = word.lower()
+        if word in data:
+            return data[word]
+        elif len(get_close_matches(word, data.keys()))>0:
+            yn = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(word, data.keys())[0])
+            if yn == 'Y' or yn == 'y':
+                return data[get_close_matches(word, data.keys())[0]]
+            elif yn == 'N' or yn == 'n':
+                return 'The word does not exist! Please check it!'
+            else:
+                return 'I did not understand the entry!'
+        else:
+            return 'The word does not exist! Please check it!'
     
 
 word = input('Enter a word: ')
 
-print(dictionary(word))
+output = dictionary(word)
+
+if type(output) == list:
+    for item in output:
+            print(item)
+else:
+    print(output)
 
 
 
