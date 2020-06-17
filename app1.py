331955d79ed9df76c2b5daa63dcd2014d686f964
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-17 11:20:21 +0200

    implemented the upper cases words

--- app1.py
+++ app1.py
@@ -8,8 +8,10 @@ def dictionary(word):
         word = word.lower()
         if word in data:
             return data[word]
-        elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        elif word.title() in data: 
             return data[word.title()]
+        elif word.upper() in data: 
+            return data[word.upper()]
         elif len(get_close_matches(word, data.keys()))>0:
             yn = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(word, data.keys())[0])
             if yn == 'Y' or yn == 'y':
