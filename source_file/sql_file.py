51b7bb601904368a112b011f8128f3a6f9e8acbc
memolinda <linda.giacomozzi@gmail.com>
2020-07-08 15:01:38 +0200

    restore old version

--- source_file/sql_file.py
+++ source_file/sql_file.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import difflib
+from difflib import get_close_matches
 
 connection = mysql.connector.connect(
     user = 'ardit700_student',
@@ -9,8 +11,18 @@ connection = mysql.connector.connect(
 
 cursor = connection.cursor()
 
-query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'line'")
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
 results = cursor.fetchall()
 
-for result in results:
-    print(result)
+
+
+if results:
+    for result in results:
+        print(result[1])
+
+# elif len(get_close_matches(word, results[0]))>0:
+#     print('Did you mean %s instead?' % get_close_matches(word, results[0])[0])
+else:
+    print("No word found!")
